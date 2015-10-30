b24818b3d64da0e2a1520fb1df52c728cdc51b9d
Jin Kato <jin.kato@starcomww.com>
2015-10-30 18:00:45 -0400

    init

---
+++ bloc_alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bloc_alcolator
+//
+//  Created by Jin Kato on 10/30/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
