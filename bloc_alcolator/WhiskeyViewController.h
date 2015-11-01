ddf1c9a62daf9945b4b93be5dc4e408904d41f2e
Jin Kato <jin.kato@starcomww.com>
2015-10-31 23:50:06 -0400

    ch20 completed

---
+++ bloc_alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  bloc_alcolator
+//
+//  Created by Jin Kato on 10/31/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
