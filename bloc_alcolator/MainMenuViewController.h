7afc5bc0651e189b098025eb6d46a4261989b2c4
Jin Kato <jin.kato@starcomww.com>
2015-11-01 12:09:21 -0500

    added the code from the excisie

---
+++ bloc_alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  bloc_alcolator
+//
+//  Created by Jin Kato on 11/1/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
